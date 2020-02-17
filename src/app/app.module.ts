13f07e09f281c03556c337f0bcbb16891ff2f8f8
unknown <abdelfattah59@gmail.com>
2020-02-17 16:37:38 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+
+// HttpClientModule sert pour communiquer avec des requetes http
+import { HttpClientModule } from '@angular/common/http';
+// FormsModule pour la gestion des formulaires
+import { FormsModule } from '@angular/forms';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TasksComponent
   ],
+  // on rajoute les imports pour qu angular les prendres en considération
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/task.ts
@@ -0,0 +1,6 @@
+export interface Task {
+    // le champ id est optionnel pour le mentinneron utilise (?)
+    id?: number;
+    label: string;
+    completed:boolean;
+}
